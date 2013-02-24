181165c544f976fe113dd465a55e034f34b99d0f
Ben Tesser <btesser@gmail.com>
2013-02-24 13:43:42 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightlife::Application.config.secret_token = '27f76c2cd170c1b412b99162ceba2ef8a5af0472073a2aa70e5594c5c981d4885911a63a9d1e33fb0a1c1e21207345c5c3362c141ccf976ccfaa2cac62cc8d16'
